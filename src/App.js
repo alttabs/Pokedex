ee942e41fa2e407a71a7da0a165351c2afbcb50a
THABATA SANTOS DE ALMEIDA <164131042@labinfo.unifacs.br>
2018-11-10 17:59:16 -0300

    varias ondas

--- src/App.js
+++ src/App.js
@@ -2,21 +2,18 @@ import React, { Component } from 'react';
 import Topbar from './componentes/Topbar';
 import PokemonCard from './componentes/PokemonCard';
 
+const listaPokemons = () => [1, 2, 3, 10, 88, 100 ,700,345, 500].map(id => 
+{return<PokemonCard key={id} pokemonId={id}/>;
+});
+
 class App extends Component {
   render() {
     return (
       <div>
         <Topbar titulo="Pokedex" cor="secondary"/>
-        <PokemonCard/>
-        <PokemonCard/>
-        <PokemonCard/>
-        <PokemonCard/>
-        <PokemonCard/>
-        <PokemonCard/>
-        <PokemonCard/>
-        <PokemonCard/>
-        
-      </div>
+        <div style={{marginTop: 72}}>
+        {listaPokemons()}</div>
+        </div>
     );
   }
 }
--- src/componentes/PokemonCard.jsx
+++ src/componentes/PokemonCard.jsx
@@ -5,8 +5,37 @@ import CardHeader from '@material-ui/core/CardHeader';
 import Avatar from '@material-ui/core/Avatar';
 
 class PokemonCard extends React.Component{
+    state = { 
+        foto:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png',
+        nome: 'eevee'
+        };    
+       
+    componentDidMount(){
+    this.carregarPokemon();
+    }
+
+    carregarPokemon = async () => {  
+        console.log(this.props.pokemonId);
+        const response = await fetch(`api/v2/pokemon/${this.props.pokemonId}/`);
+        const data = await response.json();
+        const nome = data.name;
+        const foto = data.sprites.front_default;
+        this.setState({nome, foto});
+        };
+
+    componet
     render() {
-        return <div>Pokemon</div>
+        const {foto, nome} = this.state;
+        console.log('Renderizei');
+        return (   
+            <Card style={{margin:16}}>        
+              <CardHeader
+                avatar={ <Avatar style={{ width:64, height:64}} src={foto}/>}         
+                
+                title={nome}
+              />
+            </Card>
+        )
     }
 };
 
