b4ae896542e9d9f8835712f67f1ea95ba86b9dc3
THABATA SANTOS DE ALMEIDA <164131042@labinfo.unifacs.br>
2018-11-10 15:25:37 -0300

    criando topbar

---
+++ src/componentes/Topbar.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
+
+class Topbar extends Component {
+  render() {
+    const { classes, titulo } = this.props;return (
+      <div className={classes.root}>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="h6" color="inherit">
+              {titulo}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(App);
+
