059fe2bd3fa524d5f781270a507009f120bb823b
THABATA SANTOS DE ALMEIDA <164131042@labinfo.unifacs.br>
2018-11-10 15:44:52 -0300

    componente pokemoncard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import Topbar from './componentes/Topbar';
+import PokemonCard from './componentes/PokemonCard';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Topbar titulo="Pokedex" cor="secondary"/>
+        <PokemonCard/>
+        <PokemonCard/>
+        <PokemonCard/>
+        <PokemonCard/>
+        <PokemonCard/>
+        <PokemonCard/>
+        <PokemonCard/>
+        <PokemonCard/>
+        
       </div>
     );
   }
---
+++ src/componentes/PokemonCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import Avatar from '@material-ui/core/Avatar';
+
+class PokemonCard extends React.Component{
+    render() {
+        return <div>Pokemon</div>
+    }
+};
+
+export default PokemonCard; 
